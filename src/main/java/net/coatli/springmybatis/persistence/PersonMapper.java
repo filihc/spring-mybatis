03ffb547f5eec42f0b728b1d2da5494136fa312b
b0gg4rd <boggard@hotmail.com>
2016-03-09 08:25:39 -0600

    refactor(personmapper): foreing key address

--- src/main/java/net/coatli/springmybatis/persistence/PersonMapper.java
+++ src/main/java/net/coatli/springmybatis/persistence/PersonMapper.java
@@ -3,4 +3,6 @@ package net.coatli.springmybatis.persistence;
 import net.coatli.springmybatis.domain.Person;
 
 public interface PersonMapper extends CrudMapper<Person, String> {
+
+  public Person readWithAddress(String key);
 }
